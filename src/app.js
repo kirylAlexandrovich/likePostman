b06b184be591f0b741979ea98cf3ffa4efa20da7
Kiryl <kiryl.alexandrovich@gmail.com>
2020-05-15 14:54:42 +0300

    init commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.use('/', express.static(__dirname + '/resources/static'));
+
+server.use('/', routes);
+
+module.exports = server;
